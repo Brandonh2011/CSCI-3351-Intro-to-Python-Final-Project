6ecb5b204e11496b842dd63ae4e7a42094e4932d
socialhi2 <150467326+socialhi2@users.noreply.github.com>
2025-11-05 18:01:20 -0500

    Updated Comments
    Updated Comments

--- SmartCents.py
+++ SmartCents.py
@@ -4,10 +4,11 @@ import pygame
 # pygame setup
 pygame.display.set_caption('SmartCents')
 pygame.init()
-# Screen Size
+# Screen Size and images
 x_screen = 1280
 y_screen = 720
 screen = pygame.display.set_mode((x_screen, y_screen))
+# Background with local path in asset file
 bg_image = pygame.image.load('Assets/Background/MainBackgroundSmartCents.png')
 bg_image = pygame.transform.scale(bg_image, (1280, 720))
 
@@ -15,21 +16,25 @@ bg_image = pygame.transform.scale(bg_image, (1280, 720))
 pygame.font.init()
 my_font = pygame.font.SysFont('Comic Sans MS', 30)
 
+# Title text, can use as a outline
 text_surface = my_font.render('SmartCents!', False, (0, 0, 0))
 
+#idk
 clock = pygame.time.Clock()
 running = True
 dt = 0
 x = 0
+
+# Put things to be displayed below
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    #Background image
     screen.blit(bg_image, (x, 0))
-
+    # Title Text
     screen.blit(text_surface, (x_screen/3, 0))
 
     # flip() the display to put your work on screen
