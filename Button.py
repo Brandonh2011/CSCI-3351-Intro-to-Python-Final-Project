3f72438647a2ae30d34f9b58c3eb764e43850aee
Yung Brinney <Yungbrinney3@gmail.com>
2025-11-11 13:06:38 -0500

    Sync all files from main into financial_system.py

---
+++ Button.py
@@ -0,0 +1,24 @@
+import pygame
+
+class Button:
+    def __init__(self, x, y, width, height, text, color, hover_color, font, action):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.color = color
+        self.hover_color = hover_color
+        self.action = action
+        self.font = font
+
+    def draw(self, screen):
+        mouse_pos = pygame.mouse.get_pos()
+        current_color = self.hover_color if self.rect.collidepoint(mouse_pos) else self.color
+        pygame.draw.rect(screen, current_color, self.rect)
+
+        text_surface = self.font.render(self.text, True, (255, 255, 255)) # White text
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        screen.blit(text_surface, text_rect)
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.action()
--- SmartCents.py
+++ SmartCents.py
@@ -2,6 +2,9 @@
 import pygame
 from Button import Button
 
+RED = (255,0,0)
+GREEN = (0,255,0)
+
 # pygame setup
 pygame.display.set_caption('SmartCents')
 pygame.init()
@@ -20,6 +23,12 @@ my_font = pygame.font.SysFont('Comic Sans MS', 30)
 # Title text, can use as an outline
 title_text = my_font.render('SmartCents!', False, (0, 0, 0))
 title_contain = title_text.get_rect(center=(x_screen/2, y_screen/20))
+
+turn_text = my_font.render('Age: 0', False, (0, 0, 0))
+turn_container = turn_text.get_rect(center=(x_screen/1.1, y_screen/20))
+
+money_text = my_font.render('Money: your rich', False, (0, 0, 0))
+money_container = money_text.get_rect(center=(x_screen/10, y_screen/20))
 
 footer_text = my_font.render('This is a footer', False, (0, 0, 0))
 footer_contain = footer_text.get_rect(center=(x_screen/2, y_screen/1.1))
@@ -41,7 +50,10 @@ button_width = x_screen / 4
 button_height = y_screen / 6
 button_x = 0
 button_y = y_screen - button_height
-B1 = Button(button_x,button_y,button_width,button_height,"test",(255,0,0),(0,255,0),my_font,None)
+B1 = Button(button_x,button_y,button_width,button_height,"Bank",RED, GREEN, my_font,None)
+B2 = Button(button_width, button_y, button_width, button_height, "Upgrade", RED, GREEN, my_font, None)
+B3 = Button(button_width*2, button_y, button_width, button_height, "Event Logs", RED, GREEN, my_font, None)
+B4 = Button(button_width*3, button_y, button_width, button_height, "Settings", RED, GREEN, my_font, None)
 
 # Put things to be displayed below
 while running:
@@ -51,16 +63,24 @@ while running:
     for event in pygame.event.get():
         # handle test button click (will crash)
         B1.handle_event(event)
+        B2.handle_event(event)
+        B3.handle_event(event)
+        B4.handle_event(event)
         if event.type == pygame.QUIT:
             running = False
     #Background image
     screen.blit(bg_image, (x, 0))
     # Title Text
     screen.blit(title_text, title_contain)
+    screen.blit(turn_text, turn_container)
+    screen.blit(money_text, money_container)
     screen.blit(footer_text, footer_contain)
 
     # draw test button
     B1.draw(screen)
+    B2.draw(screen)
+    B3.draw(screen)
+    B4.draw(screen)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
